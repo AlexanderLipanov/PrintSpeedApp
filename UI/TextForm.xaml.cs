dabe157e5b79865eaa2916b95056a990befacaf2
Alexandr <lipanov162@gmail.com>
2023-02-15 21:36:21 +0300

    вынес UI в отдельный Class Library

--- UI/TextForm.xaml.cs
+++ UI/TextForm.xaml.cs
@@ -1,15 +1,36 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace UI
 {
     /// <summary>
     /// Interaction logic for TextForm.xaml
     /// </summary>
-    public partial class TextForm : Page
+    public partial class TextForm 
     {
         public TextForm()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_OnChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox? textBox = sender as TextBox;
+
+            if (textBox is null) return;
+
+
+            Console.WriteLine(textBox.Text);
+        }
+
+        private void TextBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            TextBox? textBox = sender as TextBox;
+
+            if (textBox is null) return;
+
+            textBox.Clear();
         }
     }
 }
