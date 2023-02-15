9a71c2f4b9dc82249a8e35631d86202df6426ae1
Alexandr <lipanov162@gmail.com>
2023-02-15 21:13:43 +0300

    форма текста

--- PrintSpeedApp/MainWindow.xaml.cs
+++ PrintSpeedApp/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PrintSpeedApp
 {
@@ -23,6 +13,25 @@ namespace PrintSpeedApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_OnChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox? textBox = sender as TextBox;
+
+            if (textBox is null) return;
+
+
+            Console.WriteLine(textBox.Text);
+        }
+
+        private void TextBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            TextBox? textBox = sender as TextBox;
+
+            if (textBox is null) return;
+
+            textBox.Clear();
         }
     }
 }
---
+++ UI/TextForm.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for TextForm.xaml
+    /// </summary>
+    public partial class TextForm : Page
+    {
+        public TextForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
